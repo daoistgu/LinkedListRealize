736ebca6efafa5d169f253d3f7dc71ba7f35e00e
Nikita Bukov <n.bukov@appricot.com>
2023-02-12 11:14:31 +0300

    Add project files.

---
+++ LinkedListRealize/Model/MyLinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListRealize.Model;
+public class MyLinkedList<T>
+{
+    public Item<T> Head { get; private set; }
+    public Item<T> Tail { get; private set; }
+    public int Count { get; private set; }
+
+    public MyLinkedList()
+    {
+        Head = null;
+        Tail = null;
+        Count = 0;
+    }
+    public MyLinkedList(T data)
+    {
+        var item = new Item<T>(data);
+       SetHeadAndTail(data);
+    }
+
+    public void Add(T data)
+    {
+        if (Tail != null)
+        {
+            var item = new Item<T>(data);
+            Tail.Next = item;
+            Tail = item;
+            Count++;
+        }
+        else
+        {
+            SetHeadAndTail(data);
+        }
+    }
+
+    private void SetHeadAndTail(T data)
+    {
+        var item = new Item<T>(data);
+        Head = item;
+        Tail = item;
+        Count = 1;
+    }
+}
---
+++ LinkedListRealize/Program.cs
@@ -0,0 +1,8 @@
+﻿using LinkedListRealize.Model;
+
+var list = new MyLinkedList<int>();
+list.Add(1);
+list.Add(2);
+list.Add(3);
+
+Console.ReadLine();
